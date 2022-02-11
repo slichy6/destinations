82d666210a2790ee1d098105f766f219ff5c7364
slichy6 <94019317+slichy6@users.noreply.github.com>
2022-02-11 11:47:44 -0600

    unsplah

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const {destinations} = require("./db");
+const fetch = require("node-fetch");
 
-//declare express server
-const express = require("express");
-const PORT = process.env.PORT || 3000;
+function getUnsplashPhoto(keyword){
+    const url = `https://api.unsplash.com/photos/random?client_id=FqWd5kvLZXWvCnbPv0qEva4QssxcCGC94rL-f8FYxYQ&query=${keyword}`
 
-//set express server
-const server = express();
-//tell server to listen
-server.listen(PORT, () => {
-    console.log(`server is listening on PORT: ${PORT}`);
-});
-//get infornation
-server.get("/", (req, res) =>{
-    res.send(destinations);
-});
+    fetch (url).then((res) => res.json())
+    .then((data) => console.log(data.urls.small));
+
+}
+
+getUnsplashPhoto("Paris");
+
+// const {destinations} = require("./db");
+
+// //declare express server
+// const express = require("express");
+// const PORT = process.env.PORT || 3000;
+
+// //set express server
+// const server = express();
+// //tell server to listen
+// server.listen(PORT, () => {
+//     console.log(`server is listening on PORT: ${PORT}`);
+// });
+// //get infornation
+// server.get("/", (req, res) =>{
+//     res.send(destinations);
+// });
